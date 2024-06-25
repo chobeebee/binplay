fe741598da8080648f4773e0265baec5c5748a53
subin <websera12@gmail.com>
2024-06-25 11:14:48 +0900

    Test 충돌 - 삭제

--- src/test/java/com/sparta/binplay/BinplayApplicationTests.java
+++ src/test/java/com/sparta/binplay/BinplayApplicationTests.java
@@ -1,13 +1,8 @@
 package com.sparta.binplay;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BinplayApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
