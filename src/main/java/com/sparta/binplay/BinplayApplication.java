61b300d8389dc2fb82cfdb2f147eadb4accf3c77
subin <websera12@gmail.com>
2024-07-03 02:25:38 +0900

    Spring batch 테스트

--- src/main/java/com/sparta/binplay/BinplayApplication.java
+++ src/main/java/com/sparta/binplay/BinplayApplication.java
@@ -3,9 +3,13 @@ package com.sparta.binplay;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+//@EnableBatchProcessing //배치 기능 활성화 //boot 3.x부터는 필요없음
+///@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"com.sparta.binplay.repository"})
 public class BinplayApplication {
 
 	public static void main(String[] args) {
